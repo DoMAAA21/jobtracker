74b60b89ebd4b59c026e93004ac6c7f9b1011d33
DoMAAAAA21 <clashecurry21@gmail.com>
2026-05-18 10:28:24 +0800

    Implement user authentication module and update Docker configuration: add Auth and Users modules, integrate JWT and Passport dependencies, create database migration for User table, and configure pgAdmin with necessary files.

--- api/src/app.module.ts
+++ api/src/app.module.ts
@@ -1,9 +1,14 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { AuthModule } from './modules/auth/auth.module';
+import { UsersService } from './modules/users/users.service';
+import { UsersModule } from './modules/users/users.module';
 
 @Module({
   controllers: [AppController],
-  providers: [AppService],
+  providers: [AppService, UsersService],
+  imports: [AuthModule, UsersModule],
+  
 })
 export class AppModule {}
