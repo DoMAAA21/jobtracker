2e9aaef083e1fa27f5af2f91e879450a6533a802
DoMAAAAA21 <clashecurry21@gmail.com>
2026-05-18 11:00:15 +0800

    Enhance Auth module by adding 'me' endpoint to retrieve authenticated user information and refactor register/login methods to be asynchronous. Introduce AuthUser type for better type safety.

--- api/src/modules/auth/auth.controller.ts
+++ api/src/modules/auth/auth.controller.ts
@@ -1,19 +1,27 @@
-import { Controller,Post, Body } from '@nestjs/common';
+import { Controller, Post, Get, Body, Req } from '@nestjs/common';
 import { RegisterDto } from './dto/register.dto';
 import { AuthService } from './auth.service';
-import { LoginDto } from './dto/login.dto'; 
+import { LoginDto } from './dto/login.dto';
+import { AuthUser } from './types/auth-user.type';
+
+type RequestWithUser = Request & { user: AuthUser };
 
 @Controller('auth')
 export class AuthController {
-    constructor(private authService: AuthService) {}
+    constructor(private authService: AuthService) { }
 
     @Post('register')
-    register(@Body() dto: RegisterDto) {
+    async register(@Body() dto: RegisterDto) {
         return this.authService.register(dto);
     }
 
     @Post('login')
-    login(@Body() dto: LoginDto) {
+    async login(@Body() dto: LoginDto) {
         return this.authService.login(dto);
+    }
+
+    @Get('me')
+    async me(@Req() req: RequestWithUser) {
+        return req.user;
     }
 }
